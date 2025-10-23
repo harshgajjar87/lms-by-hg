45c1ae4f092b576aece7682d4581b1f50ff83e4f
Harsh Gajjar <2005harshmistry@gmail.com>
2025-10-23 12:42:12 +0530

    fix: update Penalties.js

--- frontend/src/pages/Penalties.js
+++ frontend/src/pages/Penalties.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { AuthContext } from '../context/AuthContext';
-import './Penalties.css';
+
 import { API_URL } from "../services/api";
 
 
